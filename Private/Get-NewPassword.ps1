f47f4a27f3656999e3121f65442092d648a793cc
Nick Vissari <networknerd@gmail.com>
2022-02-08 21:30:07 -0500

    Simplify Get-NewPassword

--- Private/Get-NewPassword.ps1
+++ Private/Get-NewPassword.ps1
@@ -1,20 +1,18 @@
 function Get-NewPassword {
-  $password1 = ""
-  while(-not $password1){
-    Write-Host -NoNewline "Enter new password: "
-    $Password = Read-Host -AsSecureString
-    $password1 = Get-SecureStringText -SecureString $Password
-  }
+  param(
+    [Parameter(Mandatory)]
+    [SecureString]$Password,
 
-  $password2 = ""
-  while(-not $password2){
-    Write-Host -NoNewline "Confirm new password: "
-    $confirm = Read-Host -AsSecureString
-    $password2 = Get-SecureStringText -SecureString $confirm
-  }
+    [Parameter(Mandatory)]
+    [SecureString]$ConfirmPassword
+  )
+
+  $password1 = Get-SecureStringText -SecureString $Password
+  $password2 = Get-SecureStringText -SecureString $ConfirmPassword
 
   if($password1 -ne $password2){
-    Write-Error -Message "Passwords do not match" -ErrorAction Stop
+    Throw "Passwords do not match"
   }
+
   return $Password
 }
---
+++ Tests/Get-NewPassword.Tests.ps1
@@ -0,0 +1,38 @@
+BeforeAll {
+  $ScriptName = Split-Path $PSCommandPath -Leaf
+  $PrivateDirectory = Join-Path (Split-Path $PSCommandPath | Split-Path) "Private"
+  . (Join-Path $PrivateDirectory $ScriptName.Replace('.Tests.ps1','.ps1'))
+  . (Join-Path $PrivateDirectory 'Get-SecureStringText.ps1')
+}
+
+Describe "Get-NewPassword" {
+  Context "Both passwords are the same" {
+    BeforeAll {
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force}
+      $result = Get-SecureStringText (Get-NewPassword)
+    }
+    It "Should return a string with length 64" {
+      $result | Should -Be "password"
+    }
+  }
+
+  Context "Both passwords are not the same" {
+    BeforeAll {
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force} -ParameterFilter { $Prompt -eq "Enter new password" }
+      Mock Read-Host {ConvertTo-SecureString -String "password1" -AsPlainText -Force}
+
+      $stopped = $False
+      try {
+        $result = Get-SecureStringText (Get-NewPassword)
+      } catch {
+        $stopped = $True
+      }
+    }
+    It "Should stop" {
+      $stopped | Should -Be $True
+    }
+    It "Shouldn't return anything" {
+      $result | Should -Be $null
+    }
+  }
+}
