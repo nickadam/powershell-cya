89aaf00ef0c983236eb138f9646fbb73cc236b99
Nick Vissari <networknerd@gmail.com>
2022-02-10 12:20:55 -0500

    Fix pipeline on New-CyaPassword

--- Cya/Public/New-CyaPassword.ps1
+++ Cya/Public/New-CyaPassword.ps1
@@ -48,28 +48,30 @@ function New-CyaPassword {
 
   [CmdletBinding(SupportsShouldProcess)]
   param(
-    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [String]$Name = "Default",
     [SecureString]$Password = (Get-NewPassword)
   )
 
-  $PasswordPath = Join-Path -Path (Get-CyaPasswordPath) -ChildPath $Name
+  process {
+    $PasswordPath = Join-Path -Path (Get-CyaPasswordPath) -ChildPath $Name
 
-  if(Test-Path $PasswordPath){
-    Throw "Password $Name already exists"
+    if(Test-Path $PasswordPath){
+      Throw "Password $Name already exists"
+    }
+
+    # make missing directories
+    if(-not (Test-Path (Get-CyaPasswordPath))){
+      mkdir -p (Get-CyaPasswordPath)
+    }
+
+    $Key = Get-SecureStringText $Password
+    $EncryptedBin = Get-RandomString | ConvertTo-ByteArray | Get-EncryptedBin -Password $Key
+
+    # Convert Ciphertext to base64
+    $EncryptedBin.Ciphertext = [System.Convert]::ToBase64String($EncryptedBin.Ciphertext)
+
+    # write to file
+    $EncryptedBin | ConvertTo-Json | Out-File -Encoding Default $PasswordPath
   }
-
-  # make missing directories
-  if(-not (Test-Path (Get-CyaPasswordPath))){
-    mkdir -p (Get-CyaPasswordPath)
-  }
-
-  $Key = Get-SecureStringText $Password
-  $EncryptedBin = Get-RandomString | ConvertTo-ByteArray | Get-EncryptedBin -Password $Key
-
-  # Convert Ciphertext to base64
-  $EncryptedBin.Ciphertext = [System.Convert]::ToBase64String($EncryptedBin.Ciphertext)
-
-  # write to file
-  $EncryptedBin | ConvertTo-Json | Out-File -Encoding Default $PasswordPath
 }
