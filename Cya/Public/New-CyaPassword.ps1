7ff67b95d6cd940af80838acd3d4ae34fd3e9b2b
Nick Vissari <networknerd@gmail.com>
2022-02-10 12:18:05 -0500

    Add help to New-CyaPassword

--- Cya/Public/New-CyaPassword.ps1
+++ Cya/Public/New-CyaPassword.ps1
@@ -1,4 +1,51 @@
 function New-CyaPassword {
+  <#
+  .SYNOPSIS
+  Creates CyaPasswords.
+
+  .DESCRIPTION
+  A CyaPassword is a key encrypted using AES256 with a password you supply.
+  The key, once decrypted by your password, is used to encrypt and decrypt
+  CyaConfigs. CyaPasswords provide a way to use different passwords with
+  different CyaConfigs.
+
+  .PARAMETER Name
+  [String] The name of the CyaPasswords
+
+  .PARAMETER Password
+  [SecureString] Your password to decrypt the CyaPassword
+
+  .OUTPUTS
+  $Null
+
+  .NOTES
+    Author: Nick Vissari
+
+  .EXAMPLE
+  New-CyaPassword
+
+  cmdlet New-CyaPassword at command pipeline position 1
+  Supply values for the following parameters:
+  Name: sample
+  Enter new password: *************
+  Confirm new password: *************
+
+
+  Description
+  -----------
+  Prompts for missing params
+
+  .EXAMPLE
+  New-CyaPassword -Name sample -Password (ConvertTo-SecureString -AsPlainText -Force "dont do this")
+
+
+  Description
+  -----------
+  Inscure way to set a password using plain text string. Don't do this. The
+  password will end up in your command history.
+
+  #>
+
   [CmdletBinding(SupportsShouldProcess)]
   param(
     [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
