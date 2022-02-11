b237163c5d0a62dbb71949e9df196bc75cd1ce16
Nick Vissari <networknerd@gmail.com>
2022-02-11 05:54:54 -0500

    Support pipeline on Remove-CyaConfig

--- Cya/Public/Remove-CyaConfig.ps1
+++ Cya/Public/Remove-CyaConfig.ps1
@@ -1,24 +1,37 @@
 function Remove-CyaConfig {
-  [CmdletBinding(SupportsShouldProcess)]
+  [CmdletBinding(SupportsShouldProcess,
+  DefaultParameterSetName = "FromPipeline")]
   param(
-    [Parameter(Mandatory=$true)]
-    $Name
+    [Parameter(Mandatory,
+    ParameterSetName="FromName")]
+    [String]$Name,
+
+    [Parameter(Mandatory,
+    ValueFromPipeline,
+    DontShow,
+    ParameterSetName="FromPipeline")]
+    [Object]$InputObject
   )
 
-  Get-CyaConfig -Name $Name -ErrorAction Stop | Out-Null
+  process {
+    $ConfigName = $Name
+    if(-not $Name -and $InputObject){
+      $ConfigName = $InputObject.Name
+    }
 
-  $CyaConfigPath = Get-CyaConfigPath
-  $ConfigPath = Join-Path -Path $CyaConfigPath -ChildPath $Name
+    $CyaConfigPath = Get-CyaConfigPath
+    $ConfigPath = Join-Path -Path $CyaConfigPath -ChildPath $ConfigName
 
-  # delete all bin files
-  $CyaConfig = Get-Item $ConfigPath | Get-Content | ConvertFrom-Json
-  if($CyaConfig.Files){
-    ForEach($File in $CyaConfig.Files){
-      if($File.CiphertextFile){
-        Remove-Item $File.CiphertextFile
+    # delete all bin files
+    $CyaConfig = Get-Item $ConfigPath | Get-Content | ConvertFrom-Json
+    if($CyaConfig.Files){
+      ForEach($File in $CyaConfig.Files){
+        if($File.CiphertextFile){
+          Remove-Item $File.CiphertextFile
+        }
       }
     }
-  }
 
-  Remove-Item $ConfigPath
+    Remove-Item $ConfigPath
+  }
 }
