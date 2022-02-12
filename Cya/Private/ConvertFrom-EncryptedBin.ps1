1bdbb4a499ada771724915ea6a55afa3ee8a3644
Nick Vissari <networknerd@gmail.com>
2022-02-12 10:39:39 -0500

    Add ConvertFrom-EncryptedBin

---
+++ Cya/Private/ConvertFrom-EncryptedBin.ps1
@@ -0,0 +1,85 @@
+function ConvertFrom-EncryptedBin {
+  [CmdletBinding(SupportsShouldProcess,
+  DefaultParameterSetName = "SomethingFromPipeline")]
+  param(
+    [Parameter(Mandatory)]
+    [String]$Key,
+
+    [Parameter(Mandatory, ValueFromPipeline,
+    ParameterSetName="FromFile")]
+    [String]$FileIn,
+    [Parameter(Mandatory,
+    ParameterSetName="FromFile")]
+    [String]$FileOut,
+
+    [Parameter(Mandatory,
+    ValueFromPipeline,
+    ParameterSetName="FromString")]
+    [String]$String
+  )
+
+  process {
+    $Csp =  [System.Security.Cryptography.AesCryptoServiceProvider]::New()
+    $Csp.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
+    $Csp.Mode = [System.Security.Cryptography.CipherMode]::CBC
+    $Csp.BlockSize = 128
+    $Csp.KeySize = 256
+    $SHA256 = [System.Security.Cryptography.SHA256]::Create()
+    $KeySha = $SHA256.ComputeHash([System.Text.ASCIIEncoding]::UTF8.GetBytes($Key))
+
+    if($PsCmdlet.ParameterSetName -eq "FromString"){
+      $MemoryStream = ConvertTo-MemoryStream -String $String -FromBase64
+
+      $IV = New-Object byte[] 16
+      $MemoryStream.Read($IV, 0, $IV.Length) | Out-Null
+
+      $Decryptor = $Csp.CreateDecryptor($KeySha,$IV)
+
+      $CryptoStream = [System.Security.Cryptography.CryptoStream]::New($MemoryStream, $Decryptor, [System.Security.Cryptography.CryptoStreamMode]::Read)
+      $Bytes = @()
+      do {
+        try {
+          $Byte = $CryptoStream.ReadByte()
+        } catch {
+          Throw "Failed to decrypt"
+        }
+        if($Byte -ne -1){
+          $Bytes += $Byte
+        }
+      } while ($Byte -ne -1)
+
+      try { $MemoryStream.Dispose() } catch {}
+      try { $CryptoStream.Dispose() } catch {}
+
+      [System.Text.Encoding]::UTF8.GetString($Bytes)
+    }
+
+    if($PsCmdlet.ParameterSetName -eq "FromFile"){
+      $FileInStream = [System.IO.FileStream]::New($FileIn,[System.IO.FileMode]::Open)
+      $FileOutStream = [System.IO.FileStream]::new($FileOut,[System.IO.FileMode]::Create)
+
+      $IV = New-Object byte[] 16
+      $FileInStream.Read($IV, 0, $IV.Length)
+
+      $Decryptor = $Csp.CreateDecryptor($KeySha,$IV)
+
+      $CryptoStream = [System.Security.Cryptography.CryptoStream]::New($FileInStream, $Decryptor, [System.Security.Cryptography.CryptoStreamMode]::Read)
+
+      do {
+        try {
+          $Byte = $CryptoStream.ReadByte()
+        } catch {
+          Throw
+        }
+        if($Byte -ne -1){
+          $FileOutStream.WriteByte($Byte)
+        }
+      } while ($Byte -ne -1)
+
+      try { $FileInStream.Dispose() } catch {}
+      try { $FileOutStream.Dispose() } catch {}
+      try { $CryptoStream.Dispose() } catch {}
+    }
+  }
+
+}
