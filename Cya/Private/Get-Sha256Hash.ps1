a8218790b9254c71365930a94a31339800198b7d
Nick Vissari <networknerd@gmail.com>
2022-02-12 11:51:32 -0500

    Improve file hash speed

--- Cya/Private/Get-Sha256Hash.ps1
+++ Cya/Private/Get-Sha256Hash.ps1
@@ -4,17 +4,14 @@ function Get-Sha256Hash {
   $Sha256 = [System.Security.Cryptography.HashAlgorithm]::Create("sha256")
 
   if($File){
-    $File = Get-Item $File -ErrorAction Stop
+    $hash = (Get-FileHash $File).Hash.toLower()
     if($Salt){
-      $FileBytes = [System.IO.File]::ReadAllBytes($File)
-      $SaltBytes = [System.Text.Encoding]::UTF8.getBytes($Salt)
-      $hashBytes = $Sha256.ComputeHash($SaltBytes + $FileBytes)
+      $String = $Salt + $String
+      $hashBytes = $Sha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))
       $hash = [System.BitConverter]::ToString($hashBytes)
-      $hash.toLower() -replace "-", ""
-    }else{
-      # just file path, use get-filehash
-      (Get-FileHash $File).Hash.toLower()
+      $hash = $hash.toLower() -replace "-", ""
     }
+    $hash
   }elseif($String){
     if($Salt){
       $String = $Salt + $String
