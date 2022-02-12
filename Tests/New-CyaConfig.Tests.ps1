3138c9a3dd98c3cb58060c9c19cd3ad561ea1e5f
Nick Vissari <networknerd@gmail.com>
2022-02-12 16:18:41 -0500

    Add tests to New-CyaConfig

--- Tests/New-CyaConfig.Tests.ps1
+++ Tests/New-CyaConfig.Tests.ps1
@@ -81,6 +81,87 @@ Describe "New-CyaConfig" {
     }
   }
 
+  Context "Creating files" {
+    BeforeAll {
+      $TmpFile1 = New-TemporaryFile
+      (Get-RandomString -Length 1025) | Out-File -Encoding Default -NoNewline $TmpFile1
+      $TmpFile2 = New-TemporaryFile
+      (Get-RandomString -Length 1025) | Out-File -Encoding Default -NoNewline $TmpFile2
+      $OriginalPwd = pwd
+      cd (Split-Path $TmpFile2)
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force} -ParameterFilter { $Prompt -eq "Enter new password" }
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force} -ParameterFilter { $Prompt -eq "Confirm new password" }
+      Mock Invoke-ChoicePrompt { "File" } -ParameterFilter { $Caption -eq "Config type" }
+      Mock Invoke-ChoicePrompt { "" } -ParameterFilter { $Caption -eq "Protect on exit" }
+      Mock Read-Host {$TmpFile1} -ParameterFilter { $Prompt -eq "File 1 path (Enter when done)" }
+      Mock Read-Host {Split-Path $TmpFile2 -Leaf} -ParameterFilter { $Prompt -eq "File 2 path (Enter when done)" }
+      Mock Read-Host {""} -ParameterFilter { $Prompt -eq "File 3 path (Enter when done)" }
+      $Status = New-CyaConfig -Name "test"
+    }
+    It "Should put create a CyaConfig file" {
+      (Get-Content (Join-Path $ConfigsPath "test") | ConvertFrom-Json).Type | Should -Be "File"
+    }
+    AfterAll {
+      $Env:MYVAR = ""
+      $Env:MYOTHERVAR = ""
+      Remove-Item $CYAPATH -Force -Recurse
+      Remove-Item $TmpFile1
+      Remove-Item $TmpFile2
+      cd $OriginalPwd
+    }
+  }
+
+  Context "Creating file from argument" {
+    BeforeAll {
+      $TmpFile = New-TemporaryFile
+      (Get-RandomString -Length 1025) | Out-File -Encoding Default -NoNewline $TmpFile
+      $OriginalPwd = pwd
+      cd (Split-Path $TmpFile)
+      $FileName = Split-Path $TmpFile -Leaf
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force} -ParameterFilter { $Prompt -eq "Enter new password" }
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force} -ParameterFilter { $Prompt -eq "Confirm new password" }
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force} -ParameterFilter { $Prompt -eq "Enter password for CyaPassword `"Default`"" }
+      Mock Invoke-ChoicePrompt { "" } -ParameterFilter { $Caption -eq "Protect on exit" }
+      New-CyaPassword
+      $Status = $FileName | New-CyaConfig -Name "test"
+    }
+    It "Should put create a CyaConfig file" {
+      (Get-Content (Join-Path $ConfigsPath "test") | ConvertFrom-Json).Type | Should -Be "File"
+    }
+    AfterAll {
+      $Env:MYVAR = ""
+      $Env:MYOTHERVAR = ""
+      Remove-Item $CYAPATH -Force -Recurse
+      Remove-Item $TmpFile
+      cd $OriginalPwd
+    }
+  }
+
+  Context "Creating file from param" {
+    BeforeAll {
+      $TmpFile = New-TemporaryFile
+      (Get-RandomString -Length 1025) | Out-File -Encoding Default -NoNewline $TmpFile
+      $OriginalPwd = pwd
+      cd (Split-Path $TmpFile)
+      $FileName = Split-Path $TmpFile -Leaf
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force} -ParameterFilter { $Prompt -eq "Enter new password" }
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force} -ParameterFilter { $Prompt -eq "Confirm new password" }
+      Mock Read-Host {ConvertTo-SecureString -String "password" -AsPlainText -Force} -ParameterFilter { $Prompt -eq "Enter password for CyaPassword `"Default`"" }
+      New-CyaPassword
+      $Status = New-CyaConfig -File $FileName -Name "test" -ProtectOnExit $True
+    }
+    It "Should put create a CyaConfig file" {
+      (Get-Content (Join-Path $ConfigsPath "test") | ConvertFrom-Json).Type | Should -Be "File"
+    }
+    AfterAll {
+      $Env:MYVAR = ""
+      $Env:MYOTHERVAR = ""
+      Remove-Item $CYAPATH -Force -Recurse
+      Remove-Item $TmpFile
+      cd $OriginalPwd
+    }
+  }
+
   AfterAll {
     if(Get-Item $CYAPATH -EA SilentlyContinue){
       Remove-Item $CYAPATH -Force -Recurse
